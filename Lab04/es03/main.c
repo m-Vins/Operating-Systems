d4f843c7fe1745e9ebc1762ecc23049db7ba5629
Vins <s259549@studenti.polito.it>
2020-11-10 10:08:42 +0100

    update Lab04

--- Lab04/es03/main.c
+++ Lab04/es03/main.c
@@ -5,10 +5,11 @@
 
 #define BUFFER_SIZE 32
 #define MAX_ARGUMENT 5
-#define END "end\0"
+
+#define GDB 0
 
 int main(int argc,char **argv){
-    
+    char end[]="end\0";
     FILE *fp;
     char buf[BUFFER_SIZE];
     int i=0,flag_path=0;
@@ -28,20 +29,20 @@ int main(int argc,char **argv){
         exit(0);
     }
 
-
+#if GDB
     while(fscanf(fp,"%s",buf)!=EOF){
-        if(strcmp(buf,END)!=0){
+        if(strcmp(buf,end)!=0){
             if(!flag_path){
                 strcpy(path,buf);
                 flag_path=1;
             }else {
-                cmd[i] = (char *) malloc(sizeof(char) * (strlen(buf)+1));
+                cmd[i] = (char *) malloc(sizeof(char) * BUFFER_SIZE);
                 strcpy(cmd[i], buf);
                 i++;
             }
         }else{
             cmd[i]=NULL;
-            
+            //exec
             if(!fork()){
 		        sleep(3);
                 fprintf(stdout,"Running Exec.\n");
@@ -66,6 +67,51 @@ int main(int argc,char **argv){
             flag_path=0;
         }
     }
-    fclose(fp);
+
+#else
+    char *tmpaddress;
+
+    for(int i=0;i<MAX_ARGUMENT;i++)
+        cmd[i]=(char *)malloc(BUFFER_SIZE*sizeof(char));
+
+    while(fscanf(fp,"%s",buf)!=EOF){
+        if(strcmp(buf,end)!=0){
+            if(!flag_path){
+                strcpy(path,buf);
+                flag_path=1;
+            }else {
+                strcpy(cmd[i], buf);
+                i++;
+            }
+        }else{
+            tmpaddress=cmd[i];
+            cmd[i]=NULL;
+            //exec
+            if(!fork()){
+                sleep(3);
+                fprintf(stdout,"Running Exec.\n");
+                execvp(path,cmd);
+            }else{
+                strcat(tmp,path);
+                strcat(tmp," ");
+                for(int j=0;j<i;j++){
+                    strcat(tmp,cmd[j]);
+                    if(j!=i-1)
+                        strcat(tmp," ");
+                }
+                system(tmp);
+                strcpy(tmp,"\0");
+            }
+            cmd[i]=tmpaddress;
+            i=0;
+            flag_path=0;
+        }
+    }
+
+    for(int i=0;i<MAX_ARGUMENT;i++)
+        free(cmd[i]);
+
+
+#endif
     return 0;
 }
